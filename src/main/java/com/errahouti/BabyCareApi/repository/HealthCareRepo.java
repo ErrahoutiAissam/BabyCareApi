f12c4dbb10c4d68ad59461e1e2705fda7381012c
Bouali-Salma <salmabouali2610@gmail.com>
2023-12-09 23:58:55 +0100

    Added repo annotation

--- src/main/java/com/errahouti/BabyCareApi/repository/HealthCareRepo.java
+++ src/main/java/com/errahouti/BabyCareApi/repository/HealthCareRepo.java
@@ -2,6 +2,8 @@ package com.errahouti.BabyCareApi.repository;
 
 import com.errahouti.BabyCareApi.model.HealthCare;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HealthCareRepo extends JpaRepository<HealthCare, Long> {
 }
